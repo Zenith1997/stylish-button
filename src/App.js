9338077e6064bc1a8c4e04d4149356b12263fb7b
Zenith Dabarera <dabarerazenith@gmail.com>
2023-06-26 04:17:33 +0000

    minor changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
       <div>
-        <h1>My Iframe Component</h1>
+        <button className="outer-button"></button>
       </div>
-      <button className="outer-button"></button>
-      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
