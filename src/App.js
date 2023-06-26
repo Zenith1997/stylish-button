46726835c10c9e4e3516f95513820180205181c5
Dabarera <75031785+Zenith1997@users.noreply.github.com>
2023-06-26 09:32:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <div>
+        <h1>My Iframe Component</h1>
+      </div>
+      <button className="outer-button"></button>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
